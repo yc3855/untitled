74cb550349699d3a44d4159fe0717dfdad29efb3
yc3855 <yc3855@columbia.edu>
2022-11-08 20:56:31 -0500

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "NumCpp.hpp"
+
+#include <cstdlib>
+#include <iostream>
+
+class Domain
+{
+    public:
+        int nx, nz, nt, dx, dz, dt;
+};
+
+int main() {
+
+
+//    return EXIT_SUCCESS;
+    return 0;
+}
